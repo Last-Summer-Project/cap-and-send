86665077720b887178a9331f916d50abb9a2cc73
WonJung Kim <git@won-jung.kim>
2023-05-28 00:59:10 +0900

    fix: exception

--- run.py
+++ run.py
@@ -20,17 +20,20 @@ def main():
     logging.info("Hello, Your device id is %s", device_id)
 
     while True:
-        sensor_data = sensor.read_all()
-        image_b64 = cam.capture_img()
-        logging.info("Got sensor data: %s", sensor_data)
-        req = {
-            'deviceId': device_id,
-            'temperature': sensor_data[0][0],
-            'relativeHumidity': sensor_data[0][1],
-            'soilHumidity': sensor_data[1][6],
-            'imageBase64': image_b64
-        }
-        client.post('log/write', req)
+        try:
+            sensor_data = sensor.read_all()
+            image_b64 = cam.capture_img()
+            logging.info("Got sensor data: %s", sensor_data)
+            req = {
+                'deviceId': device_id,
+                'temperature': sensor_data[0][0],
+                'relativeHumidity': sensor_data[0][1],
+                'soilHumidity': sensor_data[1][6],
+                'imageBase64': image_b64
+            }
+            client.post('log/write', req)
+        except Exception as e:
+            logging.error("Unhandled Exception while running loop: %s", e)
         time.sleep(60)
 
 
